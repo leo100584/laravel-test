db3e6c8e3fa324d9ed011f7aac838ab3ade287c6
leo <lopdal84@gmail.com>
2024-07-16 10:02:09 +0200

    uploading chriper tutorial

---
+++ chriper/resources/views/chirps/minLayout.blade.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=yes">
+    <!--<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">-->
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Landingpage for larserikopdal.no</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="bootstrap-5.0.2-dist/css/bootstrap.min.css" rel="stylesheet">
+
+
+    <link href="https://fonts.googleapis.com/css?family=Saira+Extra+Condensed:500,700" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Muli:400,400i,800,800i" rel="stylesheet">
+
+    <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+
+
+    <link href="css/resume.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles/media-queries.css">
+    <!--Noe feil under her-->
+    <link rel="icon" href="img/favicon-32x32.png" sizes="32x32" type="image/png">
+
+</head>
+
+<body id="page-top">
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="sideNav">
+    <a class="navbar-brand js-scroll-trigger" href="#page-top">
+        <span class="d-block d-lg-none">Lars Erik Opdal</span>
+        <span class="d-none d-lg-block">
+          <img class="img-fluid img-profile rounded-circle mx-auto mb-2" src="img/profile2.jpg" alt="profile-picture">
+        </span>
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link js-scroll-trigger" href="#about">About</a>
+            </li>
+        </ul>
+    </div>
+</nav>
+
+<div class="container-fluid p-0">
+
+    <section class="resume-section p-3 p-lg-5 d-flex d-column" id="about">
+        <div class="my-auto">
+            <h1 class="mb-0">
+                <span class="text-primary">Systemutvikler - </span>
+            </h1>
+            <h1 class="mb-0">Lars Erik
+                <span class="text-primary">Opdal</span>
+            </h1>
+            <div class="subheading mb-5">Hudøveien 5 · Tjøme · (+47) 902 84 216 ·
+                <a href="mailto:post@larserikopdal.no">post(at)larserikopdal.no</a>
+            </div>
+            <p class="lead mb-5">Med våre tjenester og produkter tilbyr vi en smidig, sikker og problemfri start på ditt IT-prosjekt. Vi gir deg en solid plattform for effektiv intern samhandling i din virksomhet samt en sømløs dialog med både eksisterende og nye kunder. Vår helhetlige løsning samler alt du trenger på én plattform, enten det er basert på Microsoft Office 365 eller andre ledende skytjenesteplattformer. Ved å velge oss som din samarbeidspartner, kombinerer vi vår brede kunnskap innen ulike teknologiløsninger med dyp innsikt i ulike domener, og skaper skreddersydde løsninger som passer perfekt for din situasjon.</p>
+            <!--<p class="lead mb-5">Vi er dedikerte til å være mer enn bare en leverandør – vi er din pålitelige partner gjennom hele prosessen. Fra planlegging og implementering til drift og videreutvikling, er vår kompetanse og engasjement tilgjengelig for å støtte dine mål. Velkommen til vårt team, hvor vi forplikter oss til å gjøre ditt IT-prosjekt til en suksesshistorie.</p>-->
+            <h3>Tjenester:</h3>
+            Systemutvikling - JAVA EE, C# .NET, Python (Pandas)
+            <br>Integrasjon</br>
+            Drift og support
+            <br>Data migrering</br>
+            Skytjenester - Oppsett og konfigurasjon
+            <br></br>
+            <h3>Produkter:</h3>
+            SharePoint
+            <br>Office365</br>
+            Dynamics365 CRM
+            <br></br>
+            <h4>Integrasjon og regnskap:</h4>
+            Fiken
+            <br>Conta</br>
+            Tripletex
+            <br></br>
+            <h3>Referanser:</h3>
+            Simula
+            <br>Sikt.no </br>
+            Triona AS
+            <br>Bane Nor</br>
+            Statens Veivesen
+            <br>Slamrensing AS</br>
+            <br><a href="content/LEO-CV_mars2020-ENGLISH.pdf">Check out my CV in English her.</a></br>
+            </p>
+            <div class="social-icons">
+                <a href="https://www.linkedin.com/in/lars-erik-opdal-a0095540/" target="blank">
+                    <i class="fab fa-linkedin-in"></i>
+                </a>
+                <a href="https://bitbucket.org/galileo100584/" target="blank">
+                    <i class="fab fa-github"></i>
+                </a>
+            </div>
+        </div>
+    </section>
+
+    <!-- Bootstrap core JavaScript -->
+
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <script src="js/resume.min.js"></script>
+
+</div>
+
+</body>
+
+</html>
